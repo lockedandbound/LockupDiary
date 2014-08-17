b7270b9f7a9c4aec27a7931a7004b564dda512bc
lockedandbound@outlook.com <lockedandbound@outlook.com>
2014-08-17 20:21:08 +0000

    Basic signup and login logic

--- LockupDiary/cloud/app.js
+++ LockupDiary/cloud/app.js
@@ -1,30 +1,78 @@
-
 // These two lines are required to initialize Express in Cloud Code.
 var express = require('express');
+var parseExpressHttpsRedirect = require('parse-express-https-redirect');
+var parseExpressCookieSession = require('parse-express-cookie-session');
 var app = express();
 
 // Global app configuration section
 app.set('views', 'cloud/views');  // Specify the folder to find templates
 app.set('view engine', 'ejs');    // Set the template engine
 app.use(express.bodyParser());    // Middleware for reading request body
+app.use(express.cookieParser('THIS_IS_A_SECRET'));
+app.use(parseExpressCookieSession({ cookie: { maxAge: 3600000 } }));
 
-// This is an example of hooking up a request handler with a specific request
-// path and HTTP verb using the Express routing API.
+// Routes
 app.get('/hello', function(req, res) {
   res.render('hello', { message: 'Congrats, you just set up your app!' });
 });
 
-// // Example reading from the request query string of an HTTP get request.
-// app.get('/test', function(req, res) {
-//   // GET http://example.parseapp.com/test?message=hello
-//   res.send(req.query.message);
-// });
+app.get('/', function(req, res) {
+  var currentUser = Parse.User.current();
+  if (currentUser) {
+    currentUser.fetch({
+      success: function(param) {
+        res.render('hello', {message: 'User ' + currentUser.getUsername() + ' is logged in.'})
+      },
+      error: function(error) {
+        console.log(error);
+        res.send(500, 'Error');
+      }
+    });
+  } else {
+     res.redirect('login');
+  }
+});
 
-// // Example reading from the request body of an HTTP post request.
-// app.post('/test', function(req, res) {
-//   // POST http://example.parseapp.com/test (with request body "message=hello")
-//   res.send(req.body.message);
-// });
+app.get('/signup', function(req, res) {
+  res.render('signup');
+});
+
+app.post('/signup', function(req, res) {
+  var user = new Parse.User();
+  console.log(req.body.username);
+  user.set('username', req.body.username);
+  user.set('password', req.body.password);
+  user.set('email', req.body.email);
+
+  user.signUp(null, {
+    success: function(user) {
+      res.redirect('/');
+    },
+    error: function(user, error) {
+      console.log("Error signing up: ", error);
+      res.render('signup', {message: error.message});
+    }
+  });
+});
+
+app.get('/login', function(req, res) {
+  res.render('login');
+});
+
+app.post('/login', function(req, res) {
+  Parse.User.logIn(req.body.username, req.body.password).then(function() {
+    res.redirect('/');
+  },
+  function(error) {
+    console.log('Login failed; error:' + JSON.stringify(error));
+    res.redirect('/login', {message: error.message});
+  });
+});
+
+app.get('/logout', function(req, res) {
+  Parse.User.logOut();
+  res.redirect('/');
+});
 
 // Attach the Express app to Cloud Code.
 app.listen();
