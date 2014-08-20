7d643ec4619e314adfafabc6d4edae43248a33a9
lockedandbound@outlook.com <lockedandbound@outlook.com>
2014-08-20 04:01:10 +0000

    Pass proper orgasm time data to server (note save not working properly)

--- LockupDiary/cloud/app.js
+++ LockupDiary/cloud/app.js
@@ -27,6 +27,11 @@ console.log = logConstructor(console.log);
 console.error = logConstructor(console.error);
 console.warn = logConstructor(console.warn);
 
+// Data types
+var Event = Parse.Object.extend("Event");
+var Orgasm = Parse.Object.extend("Orgasm");
+var Lockup = Parse.Object.extend("Lockup");
+
 // Routes
 app.get('/hello', function(req, res) {
   console.log('hello', 'hello', {key: 'value'});
@@ -70,6 +75,38 @@ app.post('/signup', function(req, res) {
     }
   });
 });
+
+app.post('/events', function(req, res) {
+  console.log(req.body);
+  var sortTime;
+  var event = new Event();
+  event.set('type', req.body.type);
+  if (req.body.type == 'orgasm') {
+    var orgasm = new Orgasm();
+    orgasm.set('datetime', req.body.orgasmDatetime);
+    sortTime = orgasm.get('datetime');
+    event.set('event', orgasm);
+  }
+  else {
+    throw new Error("Unrecognized type: " + type)
+  }
+  event.set(sortTime, sortTime);
+
+  event.save(null, {
+    success: function(event) {
+      // Execute any logic that should take place after the object is saved.
+      console.log('New object created with objectId: ' + event.id);
+      console.log(event);
+    },
+    error: function(event, error) {
+      // Execute any logic that should take place if the save fails.
+      // error is a Parse.Error with an error code and description.
+      console.log('Failed to create new object, with error code: ' + error.message);
+    }
+  });
+
+  res.redirect('/');
+})
 
 app.get('/login', function(req, res) {
   res.render('login', {error: null});
