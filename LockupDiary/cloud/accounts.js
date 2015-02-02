95eab43f02a7dcb78b6b2b9fc78ad8631192bf04
lockedandbound <lockedandbound@outlook.com>
2015-02-01 21:02:32 -0800

    Implements changing password

--- LockupDiary/cloud/accounts.js
+++ LockupDiary/cloud/accounts.js
@@ -1,81 +1,90 @@
 module.exports = function(app) {
 
-	app.get('/login', function(req, res) {
-		if (Parse.User.current()) {
-			return res.redirect('/');
-		}
-	  res.render('login', {error: null});
-	});
+  app.get('/login', function(req, res) {
+    if (Parse.User.current()) {
+      return res.redirect('/');
+    }
+    res.render('login', {error: null});
+  });
 
-	app.post('/login', function(req, res) {
-	  Parse.User.logIn(req.body.username, req.body.password, {
-	    success: function(user) {
-	      res.redirect('/');
-	    },
-	    error: function(user, error) {
-	      console.error('Error logging in:', error);
-	      res.render('login', {error: 'ERROR: ' + error.message});
-	    }
-	  });
-	});
+  app.post('/login', function(req, res) {
+    Parse.User.logIn(req.body.username, req.body.password, {
+      success: function(user) {
+        res.redirect('/');
+      },
+      error: function(user, error) {
+        console.error('Error logging in:', error);
+        res.render('login', {error: 'ERROR: ' + error.message});
+      }
+    });
+  });
 
-	app.get('/logout', function(req, res) {
-	  Parse.User.logOut();
-	  res.redirect('/');
-	});
+  app.get('/logout', function(req, res) {
+    Parse.User.logOut();
+    res.redirect('/');
+  });
 
-	app.get('/signup', function(req, res) {
-	  res.render('signup', {error: null});
-	});
+  app.get('/signup', function(req, res) {
+    res.render('signup', {error: null});
+  });
 
-	app.post('/signup', function(req, res) {
-	  var user = new Parse.User();
-	  user.set('username', req.body.username);
-	  user.set('password', req.body.password);
-	  user.set('email', req.body.email);
+  app.post('/signup', function(req, res) {
+    var user = new Parse.User();
+    user.set('username', req.body.username);
+    user.set('password', req.body.password);
+    user.set('email', req.body.email);
 
-	  user.signUp(null, {
-	    success: function(user) {
-	      res.redirect('/');
-	    },
-	    error: function(user, error) {
-	      console.error('Error signing up:', error);
-	      res.render('signup', {error: 'ERROR: ' + error.message});
-	    }
-	  });
-	});
+    user.signUp(null, {
+      success: function(user) {
+        res.redirect('/');
+      },
+      error: function(user, error) {
+        console.error('Error signing up:', error);
+        res.render('signup', {error: 'ERROR: ' + error.message});
+      }
+    });
+  });
 
-	app.get('/settings', function(req, res) {
-		var err = req.query.err ? 'Current password is incorrect.' : null;
-		var currentUser = Parse.User.current();
-	  if (!currentUser) {
-	    return res.redirect('/login');
-	  }
-	  currentUser.fetch().then(function() {
-	    res.render('settings', {
-	    	user: currentUser,
-	    	error: err
-	    });
-	  });
-	});
+  app.get('/settings', function(req, res) {
+    var err = req.query.err ? 'Current password is incorrect.' : null;
+    var currentUser = Parse.User.current();
+    if (!currentUser) {
+      return res.redirect('/login');
+    }
+    currentUser.fetch().then(function() {
+      res.render('settings', {
+        user: currentUser,
+        error: err
+      });
+    });
+  });
 
-	app.post('/settings/change_password', function(req, res) {
-		var user = Parse.User.current();
-	  if (!user) {
-	    return res.redirect('/login');
-	  }
-	  user.fetch().then(function() {
-	  	console.log(user.getUsername());
-	  	console.log(req.body.curr_password);
-	    Parse.User.logIn(user.getUsername(), req.body.curr_password, {
-			  success: function(loginUser) {
-			    res.redirect('/');
-			  },
-			  error: function(loginUser, error) {
-			  	res.redirect('/settings?err=bad_passsword');
-			  }
-			});
-	  });
-	});
+  app.post('/settings/change_password', function(req, res) {
+    var user = Parse.User.current();
+    if (!user) {
+      return res.send(401, {'success': false, 'message': 'No logged-in user'})
+    }
+    if (req.body.new_password != req.body.confirm_password) {
+      return res.send(400, {'success': false, 'message': 'New password entries do not match.'})
+    }
+    user.fetch().then(function() {
+      Parse.User.logIn(user.getUsername(), req.body.curr_password, {
+        success: function(loginUser) {
+          loginUser.setPassword(req.body.new_password).save(null, {
+            success: function(loginUser) {
+              res.send(200, {'success': true, 'message': 'Password changed'});
+            },
+            error: function(loginUser, error) {
+              console.log('ERROR changing password:', error);
+              res.send(500, {'success': false, 'message': 'An unknown error occurred; please try again'});
+            }
+          });
+        },
+        error: function(loginUser, error) {
+          res.send(403, {'success': false, 'message': 'Current password is incorrect'})
+        }
+      });
+    });
+  });
 
 }
--- LockupDiary/public/hello.js
+++ LockupDiary/public/hello.js
@@ -2,4 +2,40 @@ $(function() {
   $('.timestamp').each(function(index, element) {
     $(this).text(moment($.trim($(this).text())).format("dddd, MMMM Do YYYY, h:mm:ss a"));
   });
+
+  var displayErrorMessage = function(errMsg) {
+    $('#changePasswordError').text(errMsg);
+    $('#changePasswordError').css('display', 'block');
+  };
+
+  var clientSideValidate = function() {
+    if ($('#new_password').val() != $('#confirm_password').val()) {
+      displayErrorMessage('New password entries do not match.')
+      return false;
+    }
+    return true;
+  };
+
+  $('#changePasswordForm').submit(function(event) {
+    event.preventDefault();
+    if (clientSideValidate()) {
+      $.post('/settings/change_password', $('#changePasswordForm').serialize())
+        .done(function (data) {
+          alert('Password changed');
+          $('#changePasswordModal').modal('hide');
+        })
+        .fail(function (error) {
+          if (error.status == 401) {
+            window.location.replace('/login');
+          }
+          else {
+            displayErrorMessage(error.responseJSON.message);
+          }
+        });
+    }
+  });
+
+  $('#confirm_password').blur(function(e) {
+    clientSideValidate();
+  });
 })
